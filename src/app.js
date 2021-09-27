7811d87543d7bf232fc399e16241bce5f60d3cc0
Eduardo Chiaro <e.chiaro@gmail.com>
2021-09-27 06:58:47 -0700

    server setup for prod

--- src/app.js
+++ src/app.js
@@ -7,17 +7,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 const token = process.env.TOKEN_SECRET;
+const origin = process.env.NODE_ENV == 'production' ? [/eduardochiaro\.com$/] : '*';
 
 //middlewares
 const corsOptions = {
-  origin: [/\.eduardochiaro\.com$/, 'http://localhost:3000'],
+  origin,
   methods: ['POST', 'GET', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 routes(app); //register the route
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(port, () => console.log(`Server in ${process.env.NODE_ENV} on port ${port}`));
